7b8f94a3ebb38f084bc44d5d6faed32fd2089772
abeltsew <35835135+abeltsew@users.noreply.github.com>
2024-11-06 14:01:19 +0300

    Darkmode added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
+import { ModeToggle } from '@/components/ui/modeToggle';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -14,7 +16,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
@@ -39,6 +41,8 @@ export default function Home() {
             />
             Deploy now
           </a>
+          <Button>Read our docs</Button>
+          <ModeToggle />
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
